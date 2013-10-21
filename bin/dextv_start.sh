a2318810456cc7c47acebce0df9d221783c86521
Fernando Ultremare <feroult@gmail.com>
2013-10-21 09:09:00 -0200

    raspberry config

---
+++ bin/dextv_start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+KEY=0AiEZezeUULf3dHVwZi16dmNtTzRRWDVodHNuTnRUdXc
+DEXTV_HOME=/home/pi/dextv
+
+# prevent top messages saying last session is broken
+sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
+
+# dextv
+chromium-browser --kiosk chrome.tv?key=$KEY &
+
+# hide mouse pointer
+unclutter & 
+
