ac6882d9767ae5c3f89f534ada351cbdd430b9a4
Fernando Ultremare <feroult@gmail.com>
2013-10-21 00:10:44 -0200

    publishing on webstore

--- utils.js
+++
@@ -1,6 +0,0 @@
-function getURLParameter(url, name) {
-	return decodeURIComponent((new RegExp('[?|&]' + name + '='
-			+ '([^&;]+?)(&|#|;|$)').exec(url) || [ , "" ])[1]
-			.replace(/\+/g, '%20'))
-			|| null;
-}
